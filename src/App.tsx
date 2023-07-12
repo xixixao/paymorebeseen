6513c9c6f02566031d70055e3b9bc95b57bba80e
Michal Srb <srb@convex.dev>
2023-07-12 16:42:31 -0700

    built

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
           $
           <input
             type="number"
-            step="0.10"
+            step="0.01"
             placeholder="0.00"
             min="1.00"
             max="999999999.99"
